3aca92693394916594aac7ad83f4dc86fec35f3f
Tushar280 <chaudarytushar280@gmail.com>
2026-06-27 18:45:38 +0530

    Update PlayerHit.cs

--- Assets/Scripts/PlayerHit.cs
+++ Assets/Scripts/PlayerHit.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class PlayerHit : MonoBehaviour
 {
+    private int hitCount = 0;
+
+
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Player has been hit!");
-
-        Debug.Log(collision.gameObject.name + " hit the player");
-
+        //Debug.Log("Player has been hit!");
+        //Debug.Log(collision.gameObject.name + " hit the player");
+        hitCount++;
+        Debug.Log("Total hits = " + hitCount + " on player");
 
         MeshRenderer meshRenderer = collision.gameObject.GetComponent<MeshRenderer>();
         Material material = meshRenderer.material;
