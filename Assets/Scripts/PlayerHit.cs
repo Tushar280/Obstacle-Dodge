821033694119a62053b246bddb08694d6eea2fc5
Tushar280 <chaudarytushar280@gmail.com>
2026-06-27 17:53:38 +0530

    Update PlayerHit.cs

--- Assets/Scripts/PlayerHit.cs
+++ Assets/Scripts/PlayerHit.cs
@@ -7,5 +7,10 @@ public class PlayerHit : MonoBehaviour
         Debug.Log("Player has been hit!");
 
         Debug.Log(collision.gameObject.name + " hit the player");
+
+
+        MeshRenderer meshRenderer = collision.gameObject.GetComponent<MeshRenderer>();
+        Material material = meshRenderer.material;
+        material.color = Color.red;
     }
 }
