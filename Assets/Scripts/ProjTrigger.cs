f4e0e7b3a1d8381f75845db4854a3f59d8195c05
Tushar280 <chaudarytushar280@gmail.com>
2026-06-30 16:22:54 +0530

    Add projectile references to ProjTrigger system
    Add a public GameObject array field to ProjTrigger script for managing projectile instances. Adjust positions of existing projectile game objects in the scene and add a new debug visualization cube as a child of the trigger collider.

--- Assets/Scripts/ProjTrigger.cs
+++ Assets/Scripts/ProjTrigger.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class ProjTrigger : MonoBehaviour
 {
-    
+    public GameObject[] projectiles;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            foreach(GameObject projectile in projectiles)
+            {
+                projectile.SetActive(true);
+            }
+        }
+    }
 }
